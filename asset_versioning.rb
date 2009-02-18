8417aa58dfdca585077b6506fcf6cc9a2a08ae7b
ralph <ralph@rvdh.de>
2009-02-18 12:36:28 +0100

    add asset versioning script

---
+++ asset_versioning.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby -wKU
+
+require 'rubygems'
+require 'git'
+require "ftools"
+
+class Asset
+  def initialize(filename)
+    @filename = filename
+  end
+  
+  def versioned_filename
+    @filename.split(".").insert(-2, sha).join(".")
+  end
+  
+  def version_control!
+    File.copy(@filename, versioned_filename)
+    lines = []
+    File.open("index.html", "r") do |file|
+      lines = file.readlines
+    end
+    lines.each_with_index do |line, i|
+      if line.include?(@filename)
+        lines[i] = line.sub(@filename, versioned_filename)
+      end
+    end
+    File.open("index.html", "r+") do |file|
+      file << lines
+    end
+  end
+
+  private
+  def sha
+    Git.open(".").gblob(@filename).log.first.sha
+  end
+end
+
+asset_filenames = %w(stylesheets/rvdh_styles.css stylesheets/blueprint/ie.css stylesheets/blueprint/print.css)
+asset_filenames.each do |asset_filename|
+  asset = Asset.new(asset_filename)
+  asset.version_control!
+end
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-ssh cloudview.joyent.us 'cd ~/domains/rvdh.de/web/public && git pull'
-
+ssh cloudview.joyent.us 'cd ~/domains/rvdh.de/web/public && git checkout . && git clean -f . && git pull && ./asset_versioning.rb'
