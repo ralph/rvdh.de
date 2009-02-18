93186831b2494740c2450d3844ebae7108b1938b
ralph <ralph@rvdh.de>
2009-02-18 14:02:13 +0100

    adjust path

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ssh cloudview.joyent.us 'cd ~/domains/rvdh.de/web/public && git checkout . && git clean -f . && git pull && ./asset_versioning.rb'
+ssh cloudview.joyent.us 'cd ~/domains/rvdh.de/web && git checkout . && git clean -f . && git pull && ./asset_versioning.rb'
